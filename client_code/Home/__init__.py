4c49fccfaab7ca0f60f4b5b73028c65de6bbc926
liamshea91@gmail.com <liamshea91@gmail.com>
2026-03-17 01:19:13 +0000

    Created Settings; Edited ServerModule and Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -6,10 +6,28 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class Home(HomeTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    user = anvil.users.get_user()
+    if user:
+      self.lbl_welcome.text = "Welcome, " + user['username'] + "!"
+    else:
+      open_form('Login')
 
-    # Any code you write here will run before the form opens.
+  @handle("btn_new_entry", "click")
+  def btn_new_entry_click(self, **event_args):
+    open_form('NewEntry')
+
+  @handle("btn_past_entries", "click")
+  def btn_past_entries_click(self, **event_args):
+    open_form('PastEntries')
+
+  @handle("btn_settings", "click")
+  def btn_settings_click(self, **event_args):
+    open_form('Settings')
+
+  @handle("btn_logout", "click")
+  def btn_logout_click(self, **event_args):
+    anvil.users.logout()
+    open_form('Login')
--- server_code/ServerModule.py
+++ server_code/ServerModule.py
@@ -38,3 +38,24 @@ def login_with_username_or_email(username_or_email, password):
     return "success"
   except Exception as e:
     return "invalid"
+
+@anvil.server.callable
+def update_user_settings(new_username, new_email, new_password):
+  user = anvil.users.get_user()
+
+  if new_username:
+    existing = app_tables.users.get(username=new_username)
+    if existing and existing != user:
+      return "username_taken"
+    user['username'] = new_username
+
+  if new_email:
+    existing = app_tables.users.get(email=new_email)
+    if existing and existing != user:
+      return "email_taken"
+    user['email'] = new_email
+
+  if new_password:
+    anvil.users.reset_password(user['email'], new_password)
+
+  return "success"
